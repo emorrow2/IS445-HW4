be55caf87d9aeed44609c20562f2e9e12815763b
emorrow2 <60417000+emorrow2@users.noreply.github.com>
2020-04-20 19:12:55 -0700

    Add files via upload
    Added deflate and cleaned up, left in some console logs to show what's happening.

--- app.js
+++ app.js
@@ -2,9 +2,18 @@
 console.log('Hello World');
 
 document.addEventListener('keydown', inflate);
+document.addEventListener('keydown', deflate);
 document.addEventListener('keydown', feedback);
 
 function feedback() {
+    var pressCode = event.keyCode;
+    if (pressCode == 73) {
+        console.log('i was pressed');
+    }
+    else if (pressCode == 68) {
+        console.log('d was pressed');
+    }
+
     console.log('you pressed a key')
 }
 /*balloonp.addEventListener('keydown', deflate);*/
@@ -12,11 +21,11 @@ function feedback() {
 function inflate() {
     var infCode = event.keyCode;
     var size = document.getElementById("balloonp").style.fontSize;
-    if (infCode == 73) {
-        console.log('e was pressed');
+    if (infCode === 73) {
         if (parseInt(size) > 60) {
             document.getElementById("balloonp").innerHTML = '💥';
             document.removeEventListener('keydown', inflate);
+            document.removeEventListener('keydown', deflate);
             /*document.removeEventListener('keydown', deflate)*/
         }
         else {
@@ -25,6 +34,26 @@ function inflate() {
             }
             var inflateStep = parseInt(size) + 10;
             document.getElementById('balloonp').style.fontSize = inflateStep.toString() + 'px';
+        }
+    }
+}
+
+function deflate() {
+    var infCode = event.keyCode;
+    var size = document.getElementById('balloonp').style.fontSize;
+    if (infCode === 68) {
+        if (parseInt(size) <= 0) {
+            document.getElementById('balloonp').innerHTML = 'Done';
+            document.removeEventListener('keydown', deflate);
+            document.removeEventListener('keydown', inflate);
+            console.log('0px text says done, you just can not see it');
+        }
+        else {
+            if (size == '') {
+                size = 30
+            }
+            var deflateStep = parseInt(size) - 10;
+            document.getElementById('balloonp').style.fontSize = deflateStep.toString() + 'px';
         }
     }
 }
